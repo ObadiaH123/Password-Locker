673a5f3b2bf25c94eb138b7e5da9e06099807294
obadiaH123 <benard.bett@student.moringaschool.com>
2021-09-03 10:38:27 +0300

    creating user function

---
+++ run.py
@@ -0,0 +1,20 @@
+from user import User,Credentials
+def create_new_user(username,password):
+    """
+    function that create a new user
+    """
+    new_user = User(username,password)
+    return new_user
+
+def save_user(user):
+    """
+    function that save user
+    """
+    user.save_user()
+
+def display_user(user):
+    """
+    function that display the user
+    """
+    return User.display_user()
+    
