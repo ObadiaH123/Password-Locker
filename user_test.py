194ce820035eb37f6a62f5310e895f44a59a7aa6
obadiaH123 <benard.bett@student.moringaschool.com>
2021-09-03 11:10:38 +0300

    adding object initialization

--- user_test.py
+++ user_test.py
@@ -6,10 +6,17 @@ class TestUser(unittest.TestCase):
         """
         a method that run before a test
         """
-        self.new_user = User("ObadiaH","Kiprotich")
+        self.new_user = User("ObadiaH","bett@123")
 
     def tearDown(self):
         """
         a cleanup method after each test runs
         """
         User.user_list = []
+
+    def test_init(self):
+        """
+        checking if the if the object is initialized correctly
+        """
+        self.assertEqual(self.new_user.username,"ObadiaH")
+        self.assertEqual(self.new_user.password,"bett@123")
